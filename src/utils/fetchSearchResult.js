d2f0f01dbaf5e89f73124440bb35ce410efbcea4
xhxh96 <xianhao@u.nus.edu>
2019-07-20 19:14:37 -0400

    Added table width

--- src/utils/fetchSearchResult.js
+++ src/utils/fetchSearchResult.js
@@ -1,9 +1,9 @@
-import {ROOT_URL, SEARCH_PATH} from './constants';
+import { ROOT_URL, SEARCH_PATH } from './constants';
 
 function getSearchUri(searchString) {
   const queryString = `?q=${searchString}`;
 
-  return encodeURI(`${ROOT_URL}${SEARCH_PATH}${queryString}`)
+  return encodeURI(`${ROOT_URL}${SEARCH_PATH}${queryString}`);
 }
 
 export async function fetchSearchResult(searchString) {
@@ -14,19 +14,21 @@ export async function fetchSearchResult(searchString) {
       crossDomain: true,
       method: 'GET',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       }
     });
 
     const jsonResponse = await response.json();
 
-    return jsonResponse.results.map(result => ({
-      name: result.package.name,
-      description: result.package.description,
-      score: result.score,
-      links: result.package.links
-    })).sort((a, b) => b.score.final - a.score.final)
+    return jsonResponse.results
+      .map(result => ({
+        name: result.package.name,
+        description: result.package.description,
+        score: result.score,
+        links: result.package.links
+      }))
+      .sort((a, b) => b.score.final - a.score.final);
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 }
