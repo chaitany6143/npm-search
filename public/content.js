09cc23388cb434ef46ebe2102ee5aceace534ebd
Min S <mindysoh@gmail.com>
2019-07-20 20:18:33 -0400

    get selected value and open a new tab

--- public/content.js
+++ public/content.js
@@ -1,5 +1,6 @@
 chrome.runtime.onMessage.addListener(request => {
-  if (request.type === 'npmSearch') {
-    // DO SOMETHING
+  if (request.type === 'npmSearch' && window.getSelection()) {
+    let value = window.getSelection().toString();
+    window.open(`https://www.npmjs.com/package/${value}`);
   }
 });
